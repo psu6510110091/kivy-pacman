92f46313d2bad908aa88a9751d90279bd28d231d
ชนาธิป สุขพรสวรรค์ <first.sdz@gmail.com>
2023-02-19 12:22:07 +0700

    Add class Wall,GamePlay

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+
+Window.size = (1200,400)
+
+class GamePlay(Screen):
+    ps = NumericProperty(77)
+    ww = NumericProperty(1200)
+    wh = NumericProperty(400)
+
+class Wall(Widget):
+    pass
 
 class PacmanApp(App):
     def build(self):
-        return
+        return GamePlay()
 
 PacmanApp().run()
