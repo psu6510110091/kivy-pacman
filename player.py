c950c9ca1f1cbb66d3b128e7aa962c700d32f252
kittisuk temrat <6510110041@psu.ac.th>
2023-02-21 22:30:26 +0700

    bpoint in player.py

--- player.py
+++ player.py
@@ -2,6 +2,47 @@ from kivy.uix.widget import Widget
 from kivy.properties import StringProperty, NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 
+bpoint = {}
+bpoint[1] = (89,40)
+bpoint[2] = (1032,40)
+bpoint[3] = (89,283)
+bpoint[4] = (1032,283)
+
+bpoint[5] = (255,40)
+bpoint[6] = (255,283)
+
+bpoint[7] = (510,40)
+bpoint[8] = (510,283)
+
+bpoint[9] = (688,40)
+bpoint[10] = (688,40)
+
+bpoint[11] = (866,40)
+bpoint[12] = (866,283)
+
+bpoint[13] = (178,121)
+bpoint[14] = (255,121)
+bpoint[15] = (178,202)
+bpoint[16] = (255,202)
+
+bpoint[17] = (510,121)
+bpoint[18] = (599,121)
+bpoint[19] = (599,202)
+bpoint[20] = (688,202)
+
+bpoint[21] = (955,121)
+bpoint[22] = (1032,121)
+bpoint[23] = (955,202)
+bpoint[24] = (1032,202)
+
+bpoint[25] = (777,40)
+bpoint[26] = (777,121)
+
+bpoint[27] = (0,159)
+bpoint[28] = (89,159)
+bpoint[29] = (1032,159)
+bpoint[30] = (1109,159)
+
 class Player(Widget):
     pac_img = StringProperty('images/pacRight.gif')
 
