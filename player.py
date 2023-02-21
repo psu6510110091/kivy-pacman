0315f0b91cd8500a81abffe7a9ba1903fe4be29f
kittisuk temrat <6510110041@psu.ac.th>
2023-02-21 22:50:53 +0700

    maek passages list

--- player.py
+++ player.py
@@ -43,6 +43,14 @@ bpoint[28] = (89,159)
 bpoint[29] = (1032,159)
 bpoint[30] = (1109,159)
 
+passages = []
+
+passages = [bpoint[1] + bpoint[2],bpoint[2] + bpoint[4],bpoint[1] + bpoint[3],bpoint[3] + bpoint[4],\
+            bpoint[5] + bpoint[6],bpoint[7] + bpoint[8],bpoint[9] + bpoint[10],bpoint[11] + bpoint[12],\
+            bpoint[13] + bpoint[14],bpoint[15] + bpoint[16],bpoint[17] + bpoint[18],bpoint[19] + bpoint[20],\
+            bpoint[21] + bpoint[22],bpoint[23] + bpoint[24],bpoint[25] + bpoint[26],bpoint[27] + bpoint[28],\
+            bpoint[29] + bpoint[30]]
+
 class Player(Widget):
     pac_img = StringProperty('images/pacRight.gif')
 
@@ -51,15 +59,21 @@ class Player(Widget):
     velocity = ReferenceListProperty(velocity_x, velocity_y)
   
     def move(self):
-        print('moving')
-        self.pos = Vector(*self.velocity)+self.pos
-        
-        if self.velocity == [0, 1]:
-            self.pac_img = "images/pacUp.gif"
-        elif self.velocity == [0, -1]:
-            self.pac_img = "images/pacDown.gif"
-        elif self.velocity == [-1, 0]:
-            self.pac_img = "images/pacLeft.gif"
-        elif self.velocity == [1, 0]:
-            self.pac_img = "images/pacRight.gif"
+        for passage in passages :
+            if  (passage[0] <= self.velocity_x + self.pos[0] ) and \
+                (passage[2] >= self.velocity_x + self.pos[0] ) and \
+                (passage[1] <= self.velocity_y + self.pos[1] ) and \
+                (passage[3] >= self.velocity_y + self.pos[1] ):
+                
+                print('moving')
+                self.pos = Vector(*self.velocity)+self.pos
+                
+                if self.velocity == [0, 1]:
+                    self.pac_img = "images/pacUp.gif"
+                elif self.velocity == [0, -1]:
+                    self.pac_img = "images/pacDown.gif"
+                elif self.velocity == [-1, 0]:
+                    self.pac_img = "images/pacLeft.gif"
+                elif self.velocity == [1, 0]:
+                    self.pac_img = "images/pacRight.gif"
         
